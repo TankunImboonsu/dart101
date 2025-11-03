19d52e7791960568d0bad13a65e7b2e866beda03
Tankun <Tankunimboonsu@gmail.com>
2025-11-03 11:29:18 +0700

    Tankun

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,50 @@
-import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+import 'package:dart_application_1/dart_application_1.dart'
+    as dart_application_1;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+//ข้อ1
+void main() {
+  print("Welcome to Dart!");
+}
+
+//ข้อ2
+void main() {
+  var planet = "Earth";
+  print(planet);
+}
+
+//ข้อ3
+void main() {
+  int myAge = 25;
+  print(myAge);
+}
+
+//ข้อ4
+void main() {
+  double piValue = 3.14159;
+  print(piValue);
+}
+
+//ข้อ5
+void main() {
+  bool isDartFun = true;
+  print(isDartFun);
+}
+
+//ข้อ6
+void main() {
+  int a = 15, b = 4;
+  print(a + b);
+}
+
+//ข้อ7
+void main() {
+  int a = 15, b = 4;
+  print(a * b);
+}
+
+//ข้อ8
+void main() {
+  String name = "Alice";
+  int age = 28;
+  print("My name is $name and I am $age years old.");
 }
